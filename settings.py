548f391fbf7a9f7acf7da7820c25b69bfa98c13c
Хомутов Евгений <homutov@42dev.ru>
2023-06-06 16:49:52 +0500

    fix settings

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ VALID_SYMBOLS = r'^[a-zA-Z0-9]+$'
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'xxx')
